e77beb4b429a66444d7becf5fbc126906ce8dbd1
BlitzBenedikt <117511666+BlitzBenedikt@users.noreply.github.com>
2023-05-06 15:49:13 +0200

    json datei in mjs

--- client.mjs
+++ client.mjs
@@ -1,46 +1,73 @@
-var attribution = new ol.control.Attribution({
+import "ol/ol.css"
+import Map from "ol/Map"
+import View from "ol/View"
+import Feature from "ol/Feature"
+import Point from "ol/geom/Point"
+import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer"
+import { OSM, Vector as VectorSource } from "ol/source"
+import { fromLonLat } from "ol/proj"
+import Overlay from "ol/Overlay"
+import { defaults as defaultControls } from "ol/control"
+
+// JSON Daten
+const location = [
+  {
+    "name": "Deutsches Hygiene Museum",
+    "latitude": 51.0442718,
+    "longitude": 13.7447284,
+    "icon": "museum",
+    "accessibility": "Rollstulltauglich und Behindertengerechtes WC und Behindertenparkplatz",
+    "accessibilityBlind": "Blindenschrift und Leitstreifen Flyer mit Blindenschrift",
+    "accessibilityDeaf": "Angebote in Deutscher Gebärdensprache",
+    "wheelchairLatitude": 51.0445022,
+    "wheelchairLongitude": 13.7463836
+  }
+]
+
+// OpenLayers Karte
+const attribution = new ol.control.Attribution({
   collapsible: false,
 })
 
-var map = new ol.Map({
-  controls: ol.control.defaults({ attribution: false }).extend([attribution]),
+const map = new Map({
+  controls: defaultControls({ attribution: false }).extend([attribution]),
   layers: [
-    new ol.layer.Tile({
-      source: new ol.source.OSM({
+    new TileLayer({
+      source: new OSM({
         url: "https://tile.openstreetmap.de/{z}/{x}/{y}.png",
         attributions: [
-          ol.source.OSM.ATTRIBUTION,
+          OSM.ATTRIBUTION,
           'Tiles courtesy of <a href="https://geo6.be/">GEO-6</a>',
         ],
         maxZoom: 18,
       }),
     }),
   ],
-  
   target: "map",
-  view: new ol.View({
-    center: ol.proj.fromLonLat([13.743339, 51.044119]),
-    //Zoom maximum und anfang.
+  view: new View({
+    center: fromLonLat([location[0].longitude, location[0].latitude]),
     maxZoom: 18,
     zoom: 12,
   }),
 })
-var layer = new ol.layer.Vector({
-  source: new ol.source.Vector({
+
+const layer = new VectorLayer({
+  source: new VectorSource({
     features: [
-      new ol.Feature({
-        geometry: new ol.geom.Point(ol.proj.fromLonLat([13.743339, 51.044119])),
+      new Feature({
+        geometry: new Point(fromLonLat([location[0].longitude, location[0].latitude])),
       }),
     ],
   }),
 })
+
 map.addLayer(layer)
 
-var container = document.getElementById("popup")
-var content = document.getElementById("popup-content")
-var closer = document.getElementById("popup-closer")
+const container = document.getElementById("popup")
+const content = document.getElementById("popup-content")
+const closer = document.getElementById("popup-closer")
 
-var overlay = new ol.Overlay({
+const overlay = new Overlay({
   element: container,
   autoPan: true,
   autoPanAnimation: {
@@ -54,11 +81,11 @@ closer.onclick = function () {
   closer.blur()
   return false
 }
+
 map.on("singleclick", function (event) {
   if (map.hasFeatureAtPixel(event.pixel) === true) {
-    var coordinate = event.coordinate
-
-    content.innerHTML = "<b>Hello alpaka!</b><br />I am fun"
+    const coordinate = event.coordinate
+    content.innerHTML = `<b>${location[0].name}</b><br/>${location[0].accessibility}`
     overlay.setPosition(coordinate)
   } else {
     overlay.setPosition(undefined)
